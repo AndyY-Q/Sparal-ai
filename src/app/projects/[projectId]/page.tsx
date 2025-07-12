1b292f195db428cd8834270a8d942adf0ae791a1
Andy Qin <andyyqin@protonmail.com>
2025-07-11 20:19:35 -0400

    09: projects

---
+++ src/app/projects/[projectId]/page.tsx
@@ -0,0 +1,18 @@
+interface PageProps {
+  params: Promise< {
+    projectId: string;
+  }>
+};
+
+const Page = async ({ params }: PageProps) => {
+    const { projectId } = await params;
+  return (
+    <div>
+    Project id: {projectId}
+    </div>
+
+    );
+  
+}
+
+export default Page;
